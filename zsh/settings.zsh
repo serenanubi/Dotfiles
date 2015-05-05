1da502eec1cc52e9f6f0d651b3e05b202a8383a8
mcounts <mcounts@gmail.com>
2015-05-05 11:07:07 -0400

    Update settings.zsh

--- zsh/settings.zsh
+++ zsh/settings.zsh
@@ -1,1 +1,13 @@
-
+# Use vim style line editing in zsh
+bindkey -v
+# Movement
+bindkey -a 'gg' beginning-of-buffer-or-history
+bindkey -a 'G' end-of-buffer-or-history
+# Undo
+bindkey -a 'u' undo
+bindkey -a '^R' redo
+# Edit line
+bindkey -a '^V' edit-command-line
+# Backspace
+bindkey '^?' backward-delete-char
+bindkey '^H' backward-delete-char
